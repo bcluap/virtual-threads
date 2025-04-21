0e16459f27df440f40954228920617458a9c253b
Paul Carter-Brown <paul.carter-brown@ukheshe.com>
2025-04-22 00:24:35 +0200

    Initial

--- src/main/java/com/test/MainClass.java
+++ src/main/java/com/test/MainClass.java
@@ -2,10 +2,15 @@ package com.test;
 
 public class MainClass {
 
+    public static String username;
+    public static String password;
+
     public static void main(String[] args) {
         try {
-            Thread.ofVirtual().start(()->{
+            Thread.ofVirtual().start(() -> {
+                System.out.println("If you see nothing after this, then we are locked up");
                 new Test();
+                System.out.println("Great, we are not locked up. You probably didnt have -Djdk.virtualThreadScheduler.parallelism=1");
             }).join();
         } catch (InterruptedException ex) {
         }
--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -10,19 +10,13 @@ public class Test {
 
     static {
         String url = "jdbc:mysql://localhost:3306/";
-        String user = "someuser";
-        String password = "somepassword";
+        String user = "doesntmatter";
+        String password = "doesntmatter";
 
         try (Connection conn = DriverManager.getConnection(url, user, password);
              PreparedStatement stmt = conn.prepareStatement("SELECT NOW()");
              ResultSet rs = stmt.executeQuery()) {
-
-            if (rs.next()) {
-                System.out.println("Current time from DB: " + rs.getString(1));
-            }
-
         } catch (Exception e) {
-            System.err.println("Database error: " + e.getMessage());
             e.printStackTrace();
         }
     }
