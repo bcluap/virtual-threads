20fe5637b282be2b74a1922f4c9cb0d23f979f2d
Paul Carter-Brown <paul.carter-brown@ukheshe.com>
2025-04-22 12:31:24 +0200

    Just using plain socket

--- src/main/java/com/test/MainClass.java
+++ src/main/java/com/test/MainClass.java
@@ -1,63 +1,53 @@
 package com.test;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainClass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         doTest1();
         doTest2();
     }
 
-    static void doTest1() {
+    static void doTest1() throws InterruptedException {
         int threadCnt = Runtime.getRuntime().availableProcessors() - 1;
-        System.out.println("Doing a test using " + threadCnt + " virtual threads");
+        System.out.println("Doing doTest1 using " + threadCnt + " virtual threads");
         AtomicInteger cnt = new AtomicInteger(0);
         for (int i = 0; i < threadCnt; i++) {
             Thread.ofVirtual().start(() -> {
-                System.out.println("Testing with a static init doing IO");
+                System.out.println("doTest1: Testing with a static init doing IO");
                 new TestWithStaticInit1();
-                System.out.println("Great, we are not locked up");
+                System.out.println("doTest1: Great, we are not locked up");
                 cnt.incrementAndGet();
             });
         }
-        long start = System.currentTimeMillis();
-        while (cnt.get() != threadCnt) {
-            try {
-                if (System.currentTimeMillis() - start > 10000) {
-                    System.out.println("Gave up waiting for all virtual threads to finish");
-                    System.exit(1);
-                }
-                System.out.println("Waiting for all virtual threads to finish");
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-            }
-        }
+
+        Thread.sleep(5000);
+
+        Thread.ofVirtual().start(() -> {
+            System.out.println("doTest1: Great, I was scheduled");
+        }).join();
     }
-    
-     static void doTest2() {
+
+    static void doTest2() throws InterruptedException {
         int threadCnt = Runtime.getRuntime().availableProcessors();
-        System.out.println("\n\n\nDoing a test using " + threadCnt + " virtual threads");
+        System.out.println("\n\n\nDoing doTest2 using " + threadCnt + " virtual threads");
         AtomicInteger cnt = new AtomicInteger(0);
         for (int i = 0; i < threadCnt; i++) {
             Thread.ofVirtual().start(() -> {
-                System.out.println("Testing with a static init doing IO");
+                System.out.println("doTest2: Testing with a static init doing IO");
                 new TestWithStaticInit2();
-                System.out.println("Great, we are not locked up");
+                System.out.println("doTest2: Great, we are not locked up");
                 cnt.incrementAndGet();
             });
         }
-        long start = System.currentTimeMillis();
-        while (cnt.get() != threadCnt) {
-            try {
-                if (System.currentTimeMillis() - start > 10000) {
-                    System.out.println("Gave up waiting for all virtual threads to finish");
-                    System.exit(1);
-                }
-                System.out.println("Waiting for all virtual threads to finish");
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-            }
-        }
+
+        Thread.sleep(5000);
+
+        Thread.ofVirtual().start(() -> {
+            System.out.println("doTest2: Great, I was scheduled");
+        }).join();
     }
 }
